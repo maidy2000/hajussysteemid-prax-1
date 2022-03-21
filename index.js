7f2d9c6aa215eef7a284a42180cebced846b607e
Kristjan Koitla <koitlakristjan@gmail.com>
2022-03-21 19:45:28 +0200

    Add responses & address saving

--- index.js
+++ index.js
@@ -1,25 +1,60 @@
-var http = require('http');
-var server = http.createServer(async (req, res) => {
-    const decodedUrl = decodeURI(req.url);
-    if(req.method == 'GET') {
-        res.end(decodedUrl);
-        return;
-    }
-    else if (req.method == 'POST') {
-        var data = [];
-        for await (const chunk of req){
-            data.push(chunk);
-        };
-        var result = Buffer.concat(data).toString();
-        res.end(result);
-        return;
-    }
-})
+import { Low, JSONFile } from "lowdb";
+import * as http from "http";
 
-if(process.argv.length > 2) {
-    var port = process.argv[2]
-} else {
-    var port = 8080
+const HOST = process.env.HOST ? process.env.HOST : "localhost";
+const PORT = process.env.PORT ? process.env.PORT : 8080;
+const IGNORED_ADDRESSES = ["127.0.0.1"];
+
+const adapter = new JSONFile("database.json");
+const db = new Low(adapter);
+
+await db.read();
+
+const requestListener = async (req, res) => {
+  console.log(req.url);
+  if (!["/blocks", "/addresses"].includes(req.url)) {
+    res.writeHead(404, "Not Found");
+    res.end();
+    return;
+  }
+
+  if (req.method !== "GET") {
+    res.writeHead(405, "Method Not Allowed");
+    res.end();
+    return;
+  }
+
+  if (req.url === "/blocks") {
+    res.end(JSON.stringify(db.data.blocks));
+    res.writeHead(200);
+    return;
+  }
+
+  if (req.url === "/addresses") {
+    res.end(JSON.stringify(db.data.addresses));
+    res.writeHead(200);
+    await registerAddress(req.socket.remoteAddress);
+    return;
+  }
+
+  res.writeHead(500);
+  res.end();
+};
+
+const server = http.createServer(requestListener);
+server.listen(PORT, HOST, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}`);
+});
+
+async function registerAddress(address) {
+  if (db.data.addresses.includes(address)) {
+    return;
+  }
+
+  if (IGNORED_ADDRESSES.includes(address)) {
+    return;
+  }
+
+  db.data.addresses.push(address);
+  await db.write();
 }
-server.listen(port)
-console.log('Server running on port ' + port)
